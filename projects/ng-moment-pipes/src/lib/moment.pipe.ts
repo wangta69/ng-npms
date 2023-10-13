4e3d5d3f478e1355c8fc75a075ed0f4bdcf21a98
younghyeong ryu <wangta69@naver.com>
2023-10-13 16:26:00 +0900

    t
    t

--- projects/ng-moment-pipes/src/lib/moment.pipe.ts
+++ projects/ng-moment-pipes/src/lib/moment.pipe.ts
@@ -6,11 +6,8 @@ const moment = _moment;
 @Pipe({name: 'moment'})
 export class MomentPipe implements PipeTransform {
     
-    public transform(value: Date | moment.Moment, ...args: any[]): any {
-        console.log('value:', value, '...args', ...args);
-        console.log('moment', moment);
-
-        const [format] = args;
-        return moment(value).format(format);
-    }
+  public transform(value: Date | moment.Moment, ...args: any[]): any {
+    const [format] = args;
+    return moment(value).format(format);
+  }
 }
